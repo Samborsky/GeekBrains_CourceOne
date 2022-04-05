ebd3eb02518b86f4970457d5b931905a4a71f184
Samborsky <samborsky.yaroslav@yandex.ru>
2022-04-05 22:31:54 +0300

    full task

--- GeekBrains_CourceOne/main.swift
+++ GeekBrains_CourceOne/main.swift
@@ -29,10 +29,37 @@ for _ in 1...100 {
 }
 //4. удалить из массива все четные числа и числа которые  не делятся на 3
 ///через замыкание
-newArray.removeAll(where: { $0 % 2 == 0 || $0 % 3 != 0})//вариант с замыканием
+newArray.removeAll(where: { $0 % 2 == 0 || $0 % 3 != 0})
 ///через цикл for in
 for i in newArray where i % 2 == 0 || i % 3 != 0 {
     let number = newArray.firstIndex(of: i) ?? 0
     newArray.remove(at: number)
 }
+//5. написать функцию, которая добавляет в массив новое число фибоначчи и добавить туда 50 элементов
+var fibonachiArray = [0, 1]
 
+func fibonachi() {
+    for i in 2...49 {
+        print(i)
+        fibonachiArray.append(fibonachiArray[i-2] + fibonachiArray[i-1])
+    }
+}
+fibonachi()
+print(fibonachiArray)
+
+//6. заполнить массив различными простыми числами.
+var array = Array(2...100)
+
+var item = 0
+
+while item <  array.count {
+    let currentNumber = array[item]
+    var nextNumber = array.suffix(from: item + 1)
+
+    nextNumber.removeAll { $0 % currentNumber == 0}
+
+    array = array.prefix(item + 1) + Array(nextNumber)
+    item += 1
+
+}
+print(array)
